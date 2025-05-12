d65a46f096413b0ac35e6ab2ab9d78d9a096ac65
Rohit Bhure <rohitbhure.cse@gmail.com>
2025-05-12 08:40:22 +0530

    pin

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-// Animate the element with ID 'box' inside 'page1'.
-// The animation scales the element from 0 to its original size, rotates it 360 degrees, and starts after a delay of 1 second.
-gsap.from('#page1 #box', {
-    scale: 0,
-    delay: 1,
-    rotate: 360,
-});
-
-// Animate the element with ID 'box' inside 'page2'.
-// The animation scales the element from 0 to its original size and rotates it 360 degrees.
-// The scrollTrigger configuration ensures the animation is triggered when the element comes into view during scrolling.
-gsap.from('#page2 #box', {
-    scale: 0,
-    rotate: 360,
+gsap.to('#page2 h1', {
+    transform: "translateX(-150%)",
     scrollTrigger: {
-        trigger: "#page2 #box", // The element that triggers the animation.
-        scroller: "body", // The container being scrolled.
-        markers: true, // Displays markers for debugging the start and end points of the animation.
-        start: "top 60%", // Animation starts when the top of the element reaches 60% of the viewport height.
-        end: "top 50%", // Animation ends when the top of the element reaches 50% of the viewport height.
-        scrub: 2 // Smoothly scrubs the animation in sync with the scroll position.
+        trigger: '#page2',
+        scroller: "body", // corrected from "scroll" to "scroller"
+        markers: true,
+        start: "top 0%",
+        end: "bottom -150%",
+        pin: true,
+        scrub: 2,
     }
-}); 
+});
